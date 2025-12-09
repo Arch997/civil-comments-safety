473f78d72f367af55a6979b08f2577e7ccf27404
Charles  Effiom <charles.effiom@coronationtechnology.com>
2025-12-09 18:53:47 +0100

    add dynamic table query to streamlit dashboard

--- toxic_word_query.py
+++ toxic_word_query.py
@@ -1,5 +1,5 @@
 
-def toxic_word_query(table_name: str):
+def toxic_word_query():
     import streamlit as st
     import os
     from dotenv import load_dotenv
@@ -9,22 +9,34 @@ def toxic_word_query(table_name: str):
 
     db_url = os.getenv('DB_URL')
 
-    st.title("AI Safety Data Audit")
+    st.title("Word Safety Data Audit")
 
-    search_term = st.text_input("Search for a keyword: e.g. stupid, politics")
+    table_map = {
+    "Toxic Quarantine": "quarantined_toxic_data",
+    "Clean Training Data": "clean_training_data"
+    }
 
+    col1, col2 = st.columns(2)
 
-    toxic_query = pd.read_sql(
-        f"SELECT * FROM {table_name} WHERE text ILIKE '%%{search_term}%%' LIMIT 50",
-        db_url
-    )
+    with col1:
+        selected_label = st.selectbox("Select dataset to audit", list(table_map.keys()))
+        table_name = table_map[selected_label]
+    
+    with col2:
+        search_term = st.text_input("Search for a keyword: e.g. stupid, politics")
 
-    if not toxic_query.empty:
-        st.error("Found toxic entries in the db")
-        st.dataframe(toxic_query)
+    if search_term:
+        toxic_query = pd.read_sql(
+            f"SELECT * FROM {table_name} WHERE text ILIKE '%%{search_term}%%' LIMIT 50",
+            db_url
+        )
 
-    else:
-        st.success("No toxic records found in quarantine")
+        if not toxic_query.empty:
+            st.error(f"Found records in {table_name}")
+            st.dataframe(toxic_query)
+
+        else:
+            st.info(f"No records found in {table_name}")
 
 if __name__ == '__main__':
-    toxic_word_query('clean_training_data')
+    toxic_word_query()
